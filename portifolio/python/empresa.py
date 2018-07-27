154a79835c2dcb261ee43c12af5ce6ae2ddb96fd
Gustavo <gustavo.soares.cdc@gmail.com>
2018-07-27 20:14:45 -0300

    commit

--- portifolio/python/empresa.py
+++
@@ -1,9 +0,0 @@
-
-class Empresa():
-    """docstring for ClassName"""
-    def __init__(self, nome, telefone):
-        self.nome = nome
-        self.telefone = telefone
-        # super(ClassName, self).__init__()
-        # self.arg = arg
-
